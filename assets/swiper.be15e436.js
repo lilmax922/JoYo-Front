baf6c9f950cf56a34a1b75b281bf59be9c1b2df7
lilmax922 <57745091+lilmax922@users.noreply.github.com>
2023-03-01 16:34:08 +0000

    Deploying to gh-pages from @ lilmax922/JoYo-Front@045a3ff263bdb04a6180ec42101e95b4ac6144b4 🚀

---
+++ assets/swiper.be15e436.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.090b319c.js";import{S as o,a as s}from"./swiper-slide.bd283de0.js";var t=e(({app:r})=>{r.use(o),r.use(s)});export{t as default};
---
+++ assets/use-quasar.ce40f646.js
@@ -0,0 +1,1 @@
+import{i as a,ag as r}from"./index.090b319c.js";function u(){return a(r)}export{u};

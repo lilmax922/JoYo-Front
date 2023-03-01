7576497dc825f9df505ab911099331d8de0ebd1c
Max <asd88922@gmail.com>
2023-03-01 23:04:30 +0800

    update dark theme

--- quasar.config.js
+++ quasar.config.js
@@ -104,7 +104,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       config: {
-        dark: 'true'
+        dark: 'auto'
       },
       cssAddon: true,
 
